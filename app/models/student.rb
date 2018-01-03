aae981174a490533dd9e6b86d7c27a6c61c705f0
Finn <achewink@gmail.com>
2018-01-03 14:46:20 +0800

    fixed email validator

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,14 @@
 class Student < ApplicationRecord
+  validates_format_of :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
   validates :first_name, :presence => true,
                          :length => { :minimum => 1, :maximum => 20 }
+
   validates :last_name, :presence => true,
                         :length => { :minimum => 1, :maximum => 20 }
+  
   validates :email, :presence => true,
+                    :length => { :minimum => 5, :maximum => 254 },
                     :uniqueness => true
-                    :email => true
+                    # :email_format => true
 end
